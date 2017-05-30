0e6d5371cf3122c27e93149939da57ad8be02bad
enra <cudcsb@gmail.com>
2017-05-30 23:02:20 +0200

    redirect if not logged in

--- routes/waiting.js
+++ routes/waiting.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+	if (!req.user) {
+		res.redirect('/')
+		return
+	}
+
 	res.render('waiting', { user: req.user.username })
 })
 
